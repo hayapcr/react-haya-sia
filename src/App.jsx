116a7abed8644b70c5799783a93dd1a6134e249b
hayapcr <haya24si@mahasiswa.pcr.ac.id>
2026-04-21 10:14:49 +0700

    Pertemuan 6

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,33 @@ import "./assets/tailwind.css";
 import Header from "./layouts/Header";
 import Dashboard from "./pages/Dashboard";
 import Sidebar from "./layouts/Sidebar";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import NotFound from "./pages/NotFound";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div id="app-container" className="bg-gray-100 min-h-screen flex">
-            <div id="layout-wrapper" className="flex flex-row flex-1">
-                <Sidebar />
-                <div id="main-content" className="flex-1 p-4">
-                    <Header />
-                    <div className="mt-4">
-                        <Dashboard />
-                    </div>
-                    
-                </div>
-            </div>
+      <div id="layout-wrapper" className="flex flex-row flex-1">
+        <Sidebar />
+        <div id="main-content" className="flex-1 p-4">
+          <Header />
+          <div className="mt-4">
+
+            {/* <Dashboard /> */}
+            <Routes>
+              <Route path="*" element={<NotFound />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/customers" element={<Customers />} />
+            </Routes>
+          </div>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
