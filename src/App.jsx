680bc9da7896d124c3f825299b3d1c1b256ad5f7
hayapcr <haya24si@mahasiswa.pcr.ac.id>
2026-04-28 08:03:41 +0700

    P7-Main dan AuthLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./assets/tailwind.css";
-import Header from "./layouts/Header";
 import Dashboard from "./pages/Dashboard";
-import Sidebar from "./layouts/Sidebar";
 import Orders from "./pages/Orders";
 import Customers from "./pages/Customers";
 import NotFound from "./pages/NotFound";
@@ -10,35 +8,31 @@ import Error401 from "./pages/Error401";
 import Error403 from "./pages/Error403";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
 
 function App() {
   return (
-    <div id="app-container" className="bg-gray-100 min-h-screen flex">
-      <div id="layout-wrapper" className="flex flex-row flex-1">
-        <Sidebar />
-        <div id="main-content" className="flex-1 p-4">
-          <Header />
-          <div className="mt-4">
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/error-400" element={<Error400 />} />
+        <Route path="/error-401" element={<Error401 />} />
+        <Route path="/error-403" element={<Error403 />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
 
-            {/* <Dashboard /> */}
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/customers" element={<Customers />} />
-
-              {/* ✅ TAMBAHAN ERROR */}
-              <Route path="/error-400" element={<Error400 />} />
-              <Route path="/error-401" element={<Error401 />} />
-              <Route path="/error-403" element={<Error403 />} />
-
-              {/* ❗ HARUS PALING BAWAH */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-
-          </div>
-        </div>
-      </div>
-    </div>
+      <Route element={<AuthLayout/>}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/forgot" element={<Forgot/>} />
+        </Route>
+    </Routes>
   );
 }
 
